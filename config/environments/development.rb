c196b2af3b42195c5dd25effd05d718c17e40b11
JacobPickle <119011795+JacobPickle@users.noreply.github.com>
2024-10-11 11:45:18 -0500

    updating development environment config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -68,6 +68,6 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
-  config.hosts << "http://budgetbuddy.pro"
-  config.hosts << "budgetbuddy.pro"
+  config.hosts << "http://jakepickle.info"
+  config.hosts << "jakepickle.info"
 end
